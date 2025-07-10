3af2e74323a4b88b0fcad8a1f832e6c656a528fa
faustseele <etevve@gmail.com>
2025-07-10 13:09:48 +0300

    fixes npm-runs & adds preview-port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [],
   server: { port: 3000 },
+  preview: { port: 3000 },
   build: { outDir: "dist" },
   css: {
     modules: {
