cd921bedcd61d5be2b6fbf65860f8750810cf07f
faustseele <etevve@gmail.com>
2025-10-27 15:24:13 +0300

    gets rid of build-children

--- src/shared/lib/helpers/factory/functions.ts
+++ src/shared/lib/helpers/factory/functions.ts
@@ -4,28 +4,6 @@ import {
 } from "../../Component/model/children.types.ts";
 import Component from "../../Component/model/Component.ts";
 
-export function buildChildren(graph: ChildGraph): ChildGraph {
-  if (!graph) throw new Error("children-graph is not defined");
-
-  Object.keys(graph.edges).forEach((edge) => {
-    if (Array.isArray(graph.edges[edge])) {
-      for (const nodeId of graph.edges[edge]) {
-        const childNode = graph.nodes[nodeId];
-        childNode.runtime = { instance: childNode.factory(childNode.params) };
-      }
-    } else {
-      const childNode = graph.nodes[edge];
-      try {
-        childNode.runtime = { instance: childNode.factory(childNode.params) };
-      } catch (e) {
-        console.error("Error in buildChildren", e);
-      }
-    }
-  });
-
-  return graph;
-}
-
 export function getInstances<
   P extends BaseProps,
   T extends Component<P>,
