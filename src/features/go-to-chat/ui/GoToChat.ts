401521821c66d431c68d760bc24d29f99cd40fd3
faustseele <etevve@gmail.com>
2026-01-23 13:33:38 +0700

    adds letter to non-ava in gotochat

--- src/features/go-to-chat/ui/GoToChat.ts
+++ src/features/go-to-chat/ui/GoToChat.ts
@@ -12,7 +12,11 @@ export class GoToChat extends Component<GoToChatProps> {
   public getRootTagCx(configs: GoToChatConfigs): string {
     const { type, selected } = configs;
     const isNotes = type === "notes";
-    return cx(css.goToChat, isNotes && css.goToChat_note, selected && css.goToChat_selected);
+    return cx(
+      css.goToChat,
+      isNotes && css.goToChat_note,
+      selected && css.goToChat_selected,
+    );
   }
 
   public componentDidMount(): void {
@@ -21,8 +25,16 @@ export class GoToChat extends Component<GoToChatProps> {
   }
 
   public getInnerMarkup(): string {
+    const noAvatar = this.configs.avatar === "/static/avatar.png";
+    const letter = this.configs.userName.charAt(0);
+
     return /*html*/ `
-      <img class="${css.avatar}" alt="User's avatar from catalogue" src="{{ avatar }}" />
+      <div class="${css.avatarWrap}">
+        <img class="${css.avatarWrap_img}" alt="User's avatar from catalogue" src="{{ avatar }}">
+        {{#if ${noAvatar} }}
+          <span class="${css.avatarWrap_letter}"> ${letter} </span>
+        {{/if}}
+      </div>
 
       <div class="${css.goToChat__content}">
         <p class="${css.userName}">{{ userName }}</p>
