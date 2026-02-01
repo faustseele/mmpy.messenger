c448aa1b7f42c30854ccf7ace59a20cc16323de8
faustseele <etevve@gmail.com>
2026-02-01 14:59:40 +0700

    tunes mob style 4 auth

--- src/pages/errors/ui/ErrorPage.ts
+++ src/pages/errors/ui/ErrorPage.ts
@@ -2,17 +2,29 @@ import { Page } from "@pages/page/ui/Page.ts";
 import { ComponentProps } from "@shared/lib/Component/model/types.ts";
 import { ErrorProps } from "../model/types.ts";
 import css from "./errors.module.css";
+import { isMobile } from "@/shared/lib/browser/isMobile.ts";
+import { cx } from "@/shared/lib/helpers/formatting/classnames.ts";
+import cssPage from "@pages/page/ui/page.module.css";
 
 export class ErrorPage extends Page<ErrorProps> {
   constructor(props: ComponentProps<ErrorProps, ErrorPage>) {
     super(props);
   }
 
+  public getRootTagCx(): string {
+    const mobile = isMobile();
+
+    return cx(
+      cssPage.moduleWindow,
+      mobile && cssPage.moduleWindow_mobile,
+      css.moduleWindow_errors,
+    );
+  }
+
   public componentDidMount(): void {
     if (!this.children?.nodes) {
       throw new Error("children is not defined");
     }
-
   }
 
   public getInnerMarkup(): string {
